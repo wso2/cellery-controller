7bb6b3dc6e62b127d3b8d0a6829d0a630b6757f8
Deependra Ariyadewa <deependra@gmail.com>
2018-10-11 21:50:38 +0530

    Remove pub-store deployment.

--- system/control-plane/global/vick-control-plane-deploy.sh
+++ system/control-plane/global/vick-control-plane-deploy.sh
@@ -8,8 +8,8 @@
 #kubectl apply -f vick-ns-init.yaml
 
 #Create the pub-store config maps
-kubectl create configmap apim-conf --from-file=apim-configs/pub-store -n vick-system
-kubectl create configmap apim-conf-datasources --from-file=apim-configs/pub-store/datasources/ -n vick-system
+#kubectl create configmap apim-conf --from-file=apim-configs/pub-store -n vick-system
+#kubectl create configmap apim-conf-datasources --from-file=apim-configs/pub-store/datasources/ -n vick-system
 
 #Create the gw config maps
 kubectl create configmap gw-conf --from-file=apim-configs/gw -n vick-system
@@ -24,7 +24,7 @@ kubectl create configmap apim-security --from-file=apim-configs/gw/security -n v
 #kubectl create configmap auth-ext --from-file=components/org.wso2.vick.auth.extensions/target/org.wso2.vick.auth.extensions-1.0.0-SNAPSHOT.jar -n vick-system
 
 #Create credentials for docker.wso2.com
-kubectl create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=$DOCKER_REG_USER --docker-password=$DOCKER_REG_PASSWD --docker-email=$DOCKER_REG_USER_EMAIL -n vick-system
+#kubectl create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=$DOCKER_REG_USER --docker-password=$DOCKER_REG_PASSWD --docker-email=$DOCKER_REG_USER_EMAIL -n vick-system
 
 #Create apim volumes and volume claims
 #kubectl apply -f vick-apim-persistent-volumes.yaml -n vick-system
@@ -35,10 +35,10 @@ kubectl apply -f vick-apim-persistent-volumes-local.yaml -n vick-system
 kubectl apply -f vick-apim-persistent-volume-claim-local.yaml -n vick-system
 
 #Create pub-store deployment and the service
-kubectl apply -f vick-apim-pub-store.yaml -n vick-system
+#kubectl apply -f vick-apim-pub-store.yaml -n vick-system
 
 #Create pub-store ingress
-kubectl apply -f vick-apim-pub-store-ingress.yaml -n vick-system
+#kubectl apply -f vick-apim-pub-store-ingress.yaml -n vick-system
 
 #Create gateway deployment and the service
 kubectl apply -f vick-apim-gw.yaml -n vick-system
