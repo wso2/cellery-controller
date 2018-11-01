b26c59acb17b4808ae5cb3cc8a4c86736dfeff55
Nadun De Silva <nadunrds@gmail.com>
2018-11-01 10:51:19 +0530

    Add Tracing Utilities

---
+++ system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/utils/tracing/TracingUtils.js
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Tracing from "./Constants";
+
+/**
+ * Utilities used for processing Constants related data.
+ */
+class TracingUtils {
+
+
+    /**
+     * Build a tree model using a spans list.
+     *
+     * @param {Array.<Span>} spansList The spans list from which the tree should be built
+     * @returns {Span} The root span of the tree
+     */
+    static buildTree(spansList) {
+        // Finding the root spans candidates (There can be one root span or two sibling root spans)
+        const spanIdList = spansList.map((span) => span.spanId);
+        const rootSpanCandidates = spansList.filter((span) => span.parentSpanId === span.traceId
+            || !spanIdList.includes(span.parentSpanId));
+
+        // Finding the root span and initializing current span
+        let rootSpan;
+        if (rootSpanCandidates.length === 1) { // Single root
+            rootSpan = rootSpanCandidates[0];
+        } else if (rootSpanCandidates.length === 2) { // Two routes with one client span and one server span
+            let rootSpanIndex;
+            if (rootSpanCandidates[0].isSiblingOf(rootSpanCandidates[1])) {
+                if (rootSpanCandidates[0].isParentOf(rootSpanCandidates[1])) {
+                    rootSpanIndex = 0;
+                } else {
+                    rootSpanIndex = 1;
+                }
+            } else {
+                throw Error("Invalid Trace: Expected 1 root span, found two non-client kind root spans candidates.");
+            }
+            rootSpan = rootSpanCandidates[rootSpanIndex];
+            rootSpanCandidates[0].sibling = rootSpanCandidates[1];
+            rootSpanCandidates[1].sibling = rootSpanCandidates[0];
+        } else {
+            throw Error(`Invalid Trace: Expected 1 root span, found ${rootSpanCandidates.length} spans`);
+        }
+
+        // Adding references to the connected nodes
+        for (let i = 0; i < spansList.length; i++) {
+            for (let j = 0; j < spansList.length; j++) {
+                const spanA = spansList[i];
+                const spanB = spansList[j];
+                if (i !== j && (spanA.isSiblingOf(spanB) || spanA.isParentOf(spanB)
+                        || spanB.isParentOf(spanA))) {
+                    spanA.addSpanReference(spanB);
+                }
+            }
+        }
+
+        return rootSpan;
+    }
+
+    /**
+     * Traverse the span tree and label the nodes.
+     *
+     * @param {Span} rootSpan The root span of the tree
+     * @param {{name: string, version: string}} parentCell The name of the cell this span's parent belongs to
+     */
+    static labelSpanTree(rootSpan, parentCell = null) {
+        rootSpan.isSystemComponent = this.isFromSystemComponent(rootSpan);
+        if (this.isFromCellGateway(rootSpan)) {
+            rootSpan.cell = this.getCell(rootSpan);
+        } else if (!rootSpan.isSystemComponent) {
+            rootSpan.cell = parentCell;
+        }
+
+        // Traversing down the tree
+        const childrenIterator = rootSpan.children.values();
+        let currentChild = childrenIterator.next();
+        while (!currentChild.done) {
+            this.labelSpanTree(currentChild.value, rootSpan.cell);
+            currentChild = childrenIterator.next();
+        }
+    }
+
+    /**
+     * Get the cell name from cell gateway span.
+     *
+     * @param {Span} cellGatewaySpan A span belonging to the cell gateway
+     * @returns {Object} Cell details
+     */
+    static getCell(cellGatewaySpan) {
+        let cell = null;
+        if (cellGatewaySpan) {
+            const matches = cellGatewaySpan.serviceName.match(Tracing.VICK.Cell.GATEWAY_NAME_PATTERN);
+            if (Boolean(matches) && matches.length === 3) {
+                cell = {
+                    name: matches[1],
+                    version: matches[2].replace(/_/g, ".")
+                };
+            } else {
+                throw Error(`Invalid cell gateway name: ${cellGatewaySpan.serviceName}`);
+            }
+        }
+        return cell;
+    }
+
+    /**
+     * Check whether a span belongs to the cell gateway.
+     *
+     * @param {Span} span The span which should be checked
+     * @returns {boolean} True if the component to which the span belongs to is a cell gateway
+     */
+    static isFromCellGateway(span) {
+        return Boolean(span) && Tracing.VICK.Cell.GATEWAY_NAME_PATTERN.test(span.serviceName);
+    }
+
+    /**
+     * Check whether a span belongs to the System.
+     *
+     * @param {Span} span The span which should be checked
+     * @returns {boolean} True if the component to which the span belongs to is a system component
+     */
+    static isFromSystemComponent(span) {
+        return Boolean(span) && (this.isFromCellGateway(span)
+            || span.serviceName === Tracing.VICK.System.ISTIO_MIXER_NAME
+            || span.serviceName === Tracing.VICK.System.GLOBAL_GATEWAY_NAME);
+    }
+
+}
+
+export default TracingUtils;
---
+++ system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/utils/tracing/TracingUtils.test.js
@@ -0,0 +1,394 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Tracing from "./Constants";
+import Span from "./Span";
+import TracingUtils from "./TracingUtils";
+
+describe("TracingUtils", () => {
+    const globalGatewayServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-a-id",
+        parentSpanId: "trace-x-id",
+        serviceName: Tracing.VICK.System.GLOBAL_GATEWAY_NAME,
+        operationName: "get-hr-info",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 100000,
+        duration: 10000,
+        tags: {}
+    });
+    const globalGatewayClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-b-id",
+        parentSpanId: "span-a-id",
+        serviceName: Tracing.VICK.System.GLOBAL_GATEWAY_NAME,
+        operationName: "call-hr-cell",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10010,
+        duration: 9980,
+        tags: {}
+    });
+    const hrCellGatewayServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-b-id",
+        parentSpanId: "span-a-id",
+        serviceName: "src:0.0.0.hr_1_0_0_employee",
+        operationName: "call-hr-cell",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10020,
+        duration: 9960,
+        tags: {}
+    });
+    const hrCellGatewayClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-c-id",
+        parentSpanId: "span-b-id",
+        serviceName: "src:0.0.0.hr_1_0_0_employee",
+        operationName: "get-employee-data",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10030,
+        duration: 9940,
+        tags: {}
+    });
+    const employeeServiceServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-c-id",
+        parentSpanId: "span-b-id",
+        serviceName: "employee-service",
+        operationName: "get-employee-data",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10060,
+        duration: 9940,
+        tags: {}
+    });
+    const employeeServiceToIstioMixerClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-d-id",
+        parentSpanId: "span-c-id",
+        serviceName: "employee-service",
+        operationName: "is-authorized",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10060,
+        duration: 9940,
+        tags: {}
+    });
+    const istioMixerServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-d-id",
+        parentSpanId: "span-c-id",
+        serviceName: Tracing.VICK.System.ISTIO_MIXER_NAME,
+        operationName: "is-authorized",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10040,
+        duration: 9920,
+        tags: {}
+    });
+    const istioMixerWorkerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-e-id",
+        parentSpanId: "span-d-id",
+        serviceName: Tracing.VICK.System.ISTIO_MIXER_NAME,
+        operationName: "authorization",
+        startTime: 10050,
+        duration: 9900,
+        tags: {}
+    });
+    const employeeServiceToReviewsServiceClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-f-id",
+        parentSpanId: "span-c-id",
+        serviceName: "employee-service",
+        operationName: "get-reviews",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10060,
+        duration: 9940,
+        tags: {}
+    });
+    const reviewsServiceServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-f-id",
+        parentSpanId: "span-c-id",
+        serviceName: "reviews-service",
+        operationName: "get-reviews",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10000,
+        duration: 100,
+        tags: {}
+    });
+    const employeeServiceToStockOptionsCellClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-g-id",
+        parentSpanId: "span-c-id",
+        serviceName: "employee-service",
+        operationName: "get-employee-stock-options",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10060,
+        duration: 9940,
+        tags: {}
+    });
+    const stockOptionsCellGatewayServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-g-id",
+        parentSpanId: "span-c-id",
+        serviceName: "src:0.0.0.stock_options_1_0_0_employee",
+        operationName: "get-employee-stock-options",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10000,
+        duration: 100,
+        tags: {}
+    });
+    const stockOptionsCellGatewayClientSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-h-id",
+        parentSpanId: "span-g-id",
+        serviceName: "src:0.0.0.stock_options_1_0_0_employee",
+        operationName: "get-employee-stock-options",
+        kind: Tracing.Span.Kind.CLIENT,
+        startTime: 10000,
+        duration: 100,
+        tags: {}
+    });
+    const stockOptionsServiceServerSpan = new Span({
+        traceId: "trace-x-id",
+        spanId: "span-h-id",
+        parentSpanId: "span-g-id",
+        serviceName: "stock-options-service",
+        operationName: "get-employee-stock-options",
+        kind: Tracing.Span.Kind.SERVER,
+        startTime: 10000,
+        duration: 100,
+        tags: {}
+    });
+
+    describe("buildTree()", () => {
+        it("should build the tracing tree from the spans list", () => {
+            const orderedSpanList = [
+                globalGatewayServerSpan, globalGatewayClientSpan, hrCellGatewayServerSpan, hrCellGatewayClientSpan,
+                employeeServiceServerSpan, employeeServiceToIstioMixerClientSpan, istioMixerServerSpan,
+                istioMixerWorkerSpan, employeeServiceToReviewsServiceClientSpan, reviewsServiceServerSpan,
+                employeeServiceToStockOptionsCellClientSpan, stockOptionsCellGatewayServerSpan, stockOptionsCellGatewayClientSpan,
+                stockOptionsServiceServerSpan,
+            ];
+
+            // Shuffle spans list
+            const spansList = orderedSpanList.map(a => [Math.random(), a])
+                .sort((a, b) => a[0] - b[0])
+                .map(a => a[1]);
+
+            expect(TracingUtils.buildTree(spansList)).toBe(globalGatewayServerSpan);
+            expect(globalGatewayServerSpan.parent).toBeNull();
+            expect(globalGatewayServerSpan.sibling).toBeNull();
+            expect(globalGatewayServerSpan.children.size).toBe(1);
+            expect(globalGatewayServerSpan.children.has(globalGatewayClientSpan)).toBe(true);
+
+            expect(globalGatewayClientSpan.parent).toBe(globalGatewayServerSpan);
+            expect(globalGatewayClientSpan.sibling).toBe(hrCellGatewayServerSpan);
+            expect(globalGatewayClientSpan.children.size).toBe(1);
+            expect(globalGatewayClientSpan.children.has(hrCellGatewayServerSpan)).toBe(true);
+
+            expect(hrCellGatewayServerSpan.parent).toBe(globalGatewayClientSpan);
+            expect(hrCellGatewayServerSpan.sibling).toBe(globalGatewayClientSpan);
+            expect(hrCellGatewayServerSpan.children.size).toBe(1);
+            expect(hrCellGatewayServerSpan.children.has(hrCellGatewayClientSpan)).toBe(true);
+
+            expect(hrCellGatewayClientSpan.parent).toBe(hrCellGatewayServerSpan);
+            expect(hrCellGatewayClientSpan.sibling).toBe(employeeServiceServerSpan);
+            expect(hrCellGatewayClientSpan.children.size).toBe(1);
+            expect(hrCellGatewayClientSpan.children.has(employeeServiceServerSpan)).toBe(true);
+
+            expect(employeeServiceServerSpan.parent).toBe(hrCellGatewayClientSpan);
+            expect(employeeServiceServerSpan.sibling).toBe(hrCellGatewayClientSpan);
+            expect(employeeServiceServerSpan.children.size).toBe(3);
+            expect(employeeServiceServerSpan.children.has(employeeServiceToIstioMixerClientSpan)).toBe(true);
+            expect(employeeServiceServerSpan.children.has(employeeServiceToReviewsServiceClientSpan)).toBe(true);
+            expect(employeeServiceServerSpan.children.has(employeeServiceToStockOptionsCellClientSpan)).toBe(true);
+
+            expect(employeeServiceToIstioMixerClientSpan.parent).toBe(employeeServiceServerSpan);
+            expect(employeeServiceToIstioMixerClientSpan.sibling).toBe(istioMixerServerSpan);
+            expect(employeeServiceToIstioMixerClientSpan.children.size).toBe(1);
+            expect(employeeServiceToIstioMixerClientSpan.children.has(istioMixerServerSpan)).toBe(true);
+
+            expect(istioMixerServerSpan.parent).toBe(employeeServiceToIstioMixerClientSpan);
+            expect(istioMixerServerSpan.sibling).toBe(employeeServiceToIstioMixerClientSpan);
+            expect(istioMixerServerSpan.children.size).toBe(1);
+            expect(istioMixerServerSpan.children.has(istioMixerWorkerSpan)).toBe(true);
+
+            expect(istioMixerWorkerSpan.parent).toBe(istioMixerServerSpan);
+            expect(istioMixerWorkerSpan.sibling).toBe(null);
+            expect(istioMixerWorkerSpan.children.size).toBe(0);
+
+            expect(employeeServiceToReviewsServiceClientSpan.parent).toBe(employeeServiceServerSpan);
+            expect(employeeServiceToReviewsServiceClientSpan.sibling).toBe(reviewsServiceServerSpan);
+            expect(employeeServiceToReviewsServiceClientSpan.children.size).toBe(1);
+            expect(employeeServiceToReviewsServiceClientSpan.children.has(reviewsServiceServerSpan)).toBe(true);
+
+            expect(reviewsServiceServerSpan.parent).toBe(employeeServiceToReviewsServiceClientSpan);
+            expect(reviewsServiceServerSpan.sibling).toBe(employeeServiceToReviewsServiceClientSpan);
+            expect(reviewsServiceServerSpan.children.size).toBe(0);
+
+            expect(employeeServiceToStockOptionsCellClientSpan.parent).toBe(employeeServiceServerSpan);
+            expect(employeeServiceToStockOptionsCellClientSpan.sibling).toBe(stockOptionsCellGatewayServerSpan);
+            expect(employeeServiceToStockOptionsCellClientSpan.children.size).toBe(1);
+            expect(employeeServiceToStockOptionsCellClientSpan.children.has(stockOptionsCellGatewayServerSpan)).toBe(true);
+
+            expect(stockOptionsCellGatewayServerSpan.parent).toBe(employeeServiceToStockOptionsCellClientSpan);
+            expect(stockOptionsCellGatewayServerSpan.sibling).toBe(employeeServiceToStockOptionsCellClientSpan);
+            expect(stockOptionsCellGatewayServerSpan.children.size).toBe(1);
+            expect(stockOptionsCellGatewayServerSpan.children.has(stockOptionsCellGatewayClientSpan)).toBe(true);
+
+            expect(stockOptionsCellGatewayClientSpan.parent).toBe(stockOptionsCellGatewayServerSpan);
+            expect(stockOptionsCellGatewayClientSpan.sibling).toBe(stockOptionsServiceServerSpan);
+            expect(stockOptionsCellGatewayClientSpan.children.size).toBe(1);
+            expect(stockOptionsCellGatewayClientSpan.children.has(stockOptionsServiceServerSpan)).toBe(true);
+
+            expect(stockOptionsServiceServerSpan.parent).toBe(stockOptionsCellGatewayClientSpan);
+            expect(stockOptionsServiceServerSpan.sibling).toBe(stockOptionsCellGatewayClientSpan);
+            expect(stockOptionsServiceServerSpan.children.size).toBe(0);
+        });
+    });
+
+
+    describe("labelSpanTree()", () => {
+        it("should label the necessary nodes according to cell, component type", () => {
+            TracingUtils.labelSpanTree(globalGatewayServerSpan);
+
+            expect(globalGatewayServerSpan.cell).toBe(null);
+            expect(globalGatewayServerSpan.isSystemComponent).toBe(true);
+
+            expect(globalGatewayClientSpan.cell).toBe(null);
+            expect(globalGatewayClientSpan.isSystemComponent).toBe(true);
+
+            expect(hrCellGatewayServerSpan.cell).not.toBeNull();
+            expect(hrCellGatewayServerSpan.cell.name).toBe("hr");
+            expect(hrCellGatewayServerSpan.cell.version).toBe("1.0.0");
+            expect(hrCellGatewayServerSpan.isSystemComponent).toBe(true);
+
+            expect(hrCellGatewayClientSpan.cell).not.toBeNull();
+            expect(hrCellGatewayClientSpan.cell.name).toBe("hr");
+            expect(hrCellGatewayClientSpan.cell.version).toBe("1.0.0");
+            expect(hrCellGatewayClientSpan.isSystemComponent).toBe(true);
+
+            expect(employeeServiceServerSpan.cell).not.toBeNull();
+            expect(employeeServiceServerSpan.cell.name).toBe("hr");
+            expect(employeeServiceServerSpan.cell.version).toBe("1.0.0");
+            expect(employeeServiceServerSpan.isSystemComponent).toBe(false);
+
+            expect(employeeServiceToIstioMixerClientSpan.cell).not.toBeNull();
+            expect(employeeServiceToIstioMixerClientSpan.cell.name).toBe("hr");
+            expect(employeeServiceToIstioMixerClientSpan.cell.version).toBe("1.0.0");
+            expect(employeeServiceToIstioMixerClientSpan.isSystemComponent).toBe(false);
+
+            expect(istioMixerServerSpan.cell).toBeNull();
+            expect(istioMixerServerSpan.isSystemComponent).toBe(true);
+
+            expect(istioMixerWorkerSpan.cell).toBeNull();
+            expect(istioMixerWorkerSpan.isSystemComponent).toBe(true);
+
+            expect(employeeServiceToReviewsServiceClientSpan.cell).not.toBeNull();
+            expect(employeeServiceToReviewsServiceClientSpan.cell.name).toBe("hr");
+            expect(employeeServiceToReviewsServiceClientSpan.cell.version).toBe("1.0.0");
+            expect(employeeServiceToReviewsServiceClientSpan.isSystemComponent).toBe(false);
+
+            expect(reviewsServiceServerSpan.cell).not.toBeNull();
+            expect(reviewsServiceServerSpan.cell.name).toBe("hr");
+            expect(reviewsServiceServerSpan.cell.version).toBe("1.0.0");
+            expect(reviewsServiceServerSpan.isSystemComponent).toBe(false);
+
+            expect(employeeServiceToStockOptionsCellClientSpan.cell).not.toBeNull();
+            expect(employeeServiceToStockOptionsCellClientSpan.cell.name).toBe("hr");
+            expect(employeeServiceToStockOptionsCellClientSpan.cell.version).toBe("1.0.0");
+            expect(employeeServiceToStockOptionsCellClientSpan.isSystemComponent).toBe(false);
+
+            expect(stockOptionsCellGatewayServerSpan.cell).not.toBeNull();
+            expect(stockOptionsCellGatewayServerSpan.cell.name).toBe("stock_options");
+            expect(stockOptionsCellGatewayServerSpan.cell.version).toBe("1.0.0");
+            expect(stockOptionsCellGatewayServerSpan.isSystemComponent).toBe(true);
+
+            expect(stockOptionsCellGatewayClientSpan.cell).not.toBeNull();
+            expect(stockOptionsCellGatewayClientSpan.cell.name).toBe("stock_options");
+            expect(stockOptionsCellGatewayClientSpan.cell.version).toBe("1.0.0");
+            expect(stockOptionsCellGatewayClientSpan.isSystemComponent).toBe(true);
+
+            expect(stockOptionsServiceServerSpan.cell).not.toBeNull();
+            expect(stockOptionsServiceServerSpan.cell.name).toBe("stock_options");
+            expect(stockOptionsServiceServerSpan.cell.version).toBe("1.0.0");
+            expect(stockOptionsServiceServerSpan.isSystemComponent).toBe(false);
+        });
+    });
+
+    describe("getCell()", () => {
+        it("should return the cell information if the span is from a Cell Gateway", () => {
+            const cell = TracingUtils.getCell(hrCellGatewayServerSpan);
+
+            expect(cell.name).toBe("hr");
+            expect(cell.version).toBe("1.0.0");
+        });
+
+        it("should throw error if the span is not not from a Cell Gateway", () => {
+            expect(() => TracingUtils.getCell(globalGatewayServerSpan)).toThrow();
+            expect(() => TracingUtils.getCell(istioMixerServerSpan)).toThrow();
+            expect(() => TracingUtils.getCell(employeeServiceServerSpan)).toThrow();
+        });
+
+        it("should return null if null is provided", () => {
+            expect(TracingUtils.getCell(null)).toBeNull();
+            expect(TracingUtils.getCell(undefined)).toBeNull();
+        });
+    });
+
+    describe("isFromCellGateway()", () => {
+        it("should return true if the span is from a Cell Gateway", () => {
+            expect(TracingUtils.isFromCellGateway(hrCellGatewayServerSpan)).toBe(true);
+        });
+
+        it("should return false if the span is not from a Cell Gateway", () => {
+            expect(TracingUtils.isFromCellGateway(globalGatewayServerSpan)).toBe(false);
+            expect(TracingUtils.isFromCellGateway(istioMixerServerSpan)).toBe(false);
+            expect(TracingUtils.isFromCellGateway(employeeServiceServerSpan)).toBe(false);
+        });
+
+        it("should return false if null is provided", () => {
+            expect(TracingUtils.isFromCellGateway(null)).toBe(false);
+            expect(TracingUtils.isFromCellGateway(undefined)).toBe(false);
+        });
+    });
+
+    describe("isFromSystemComponent()", () => {
+        it("should return true if the span is from Global Gateway", () => {
+            expect(TracingUtils.isFromSystemComponent(globalGatewayServerSpan)).toBe(true);
+        });
+
+        it("should return true if the span is from a Cell Gateway", () => {
+            expect(TracingUtils.isFromSystemComponent(hrCellGatewayServerSpan)).toBe(true);
+        });
+
+        it("should return true if the span is from Istio Mixer", () => {
+            expect(TracingUtils.isFromSystemComponent(istioMixerServerSpan)).toBe(true);
+        });
+
+        it("should return false if the span is from a custom service", () => {
+            expect(TracingUtils.isFromSystemComponent(employeeServiceServerSpan)).toBe(false);
+        });
+
+        it("should return false if null is provided", () => {
+            expect(TracingUtils.isFromSystemComponent(null)).toBe(false);
+            expect(TracingUtils.isFromSystemComponent(undefined)).toBe(false);
+        });
+    });
+
+});
