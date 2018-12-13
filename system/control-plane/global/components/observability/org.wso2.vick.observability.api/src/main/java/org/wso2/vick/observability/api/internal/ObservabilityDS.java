53cecd090606ffe7a56ffecd1d4d287d5ece9318
sinthuja <sinthuja@wso2.com>
2018-12-13 12:46:10 +0530

    Add back the model manager osgi service.

--- system/control-plane/global/components/observability/org.wso2.vick.observability.api/src/main/java/org/wso2/vick/observability/api/internal/ObservabilityDS.java
+++ system/control-plane/global/components/observability/org.wso2.vick.observability.api/src/main/java/org/wso2/vick/observability/api/internal/ObservabilityDS.java
@@ -32,6 +32,7 @@ import org.wso2.vick.observability.api.DistributedTracingAPI;
 import org.wso2.vick.observability.api.exception.mapper.ServerErrorExceptionMapper;
 import org.wso2.vick.observability.api.interceptor.CORSInterceptor;
 import org.wso2.vick.observability.api.siddhi.SiddhiStoreQueryManager;
+import org.wso2.vick.observability.model.generator.ModelManager;
 
 /**
  * This is the declarative service component of the observability API component,
@@ -125,5 +126,20 @@ public class ObservabilityDS {
     }
 
     protected void unsetDataSourceService(DataSourceService dataSourceService) {
+    }
+
+    @Reference(
+            name = "org.wso2.vick.observability.model.generator.ModelManager",
+            service = ModelManager.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetModelManager"
+    )
+    protected void setModelManager(ModelManager modelManager) {
+        ServiceHolder.setModelManager(modelManager);
+    }
+
+    protected void unsetModelManager(ModelManager modelManager) {
+        ServiceHolder.setCarbonRuntime(null);
     }
 }
