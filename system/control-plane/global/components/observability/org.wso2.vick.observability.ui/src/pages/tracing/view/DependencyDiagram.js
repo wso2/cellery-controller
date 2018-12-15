72116756b0d133e07c2e7bfd6d35ecfc330afade
Nadun De Silva <nadunrds@gmail.com>
2018-12-15 12:15:50 +0530

    Implement error indicator in Trace View Dependency Diagram

--- system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/pages/tracing/view/DependencyDiagram.js
+++ system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/pages/tracing/view/DependencyDiagram.js
@@ -31,89 +31,153 @@ const styles = () => ({
     }
 });
 
-const DependencyDiagram = (props) => {
-    const {classes, spans, colorGenerator} = props;
-    const rootSpan = TracingUtils.buildTree(spans);
+class DependencyDiagram extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const nodeIdList = [];
-    const nodes = [];
-    const links = [];
-    const getUniqueNodeId = (span) => `${span.cell ? `${span.cell.name}-` : ""}${span.serviceName}`;
-    const addNodeIfNotPresent = (span) => {
-        if (!nodeIdList.includes(getUniqueNodeId(span))) {
-            // Finding the proper color for this item
-            let colorKey = span.cell ? span.cell.name : null;
-            if (!colorKey) {
-                if (span.componentType === Constants.ComponentType.VICK) {
-                    colorKey = ColorGenerator.VICK;
-                } else if (span.componentType === Constants.ComponentType.ISTIO) {
-                    colorKey = ColorGenerator.ISTIO;
-                } else {
-                    colorKey = span.componentType;
+        this.nodes = [];
+        this.links = [];
+    }
+
+    // shouldComponentUpdate = () => false;
+
+    draw = () => {
+
+    };
+
+    render = () => {
+        const {classes, spans, colorGenerator} = this.props;
+        const rootSpan = TracingUtils.buildTree(spans);
+        const MIN_RADIUS = 40;
+        const MAX_RADIUS = 120;
+
+        let minDuration = Number.MAX_SAFE_INTEGER;
+        let maxDuration = 0;
+        const nodeIdList = [];
+        const nodes = [];
+        const links = [];
+        const getUniqueNodeId = (span) => (
+            `${span.cell && span.cell.name && !span.isFromCellGateway() ? `${span.cell.name}:` : ""}${span.serviceName}`
+        );
+        const addNodeIfNotPresent = (span) => {
+            if (!nodeIdList.includes(getUniqueNodeId(span))) {
+                // Finding the proper color for this item
+                let colorKey = span.cell ? span.cell.name : null;
+                if (!colorKey) {
+                    if (span.componentType === Constants.ComponentType.VICK) {
+                        colorKey = ColorGenerator.VICK;
+                    } else if (span.componentType === Constants.ComponentType.ISTIO) {
+                        colorKey = ColorGenerator.ISTIO;
+                    } else {
+                        colorKey = span.componentType;
+                    }
                 }
+                const color = colorGenerator.getColor(colorKey);
+
+                nodeIdList.push(getUniqueNodeId(span));
+                nodes.push({
+                    id: getUniqueNodeId(span),
+                    color: color,
+                    size: 350,
+                    span: span
+                });
             }
-            const color = colorGenerator.getColor(colorKey);
-
-            nodeIdList.push(getUniqueNodeId(span));
-            nodes.push({
-                id: getUniqueNodeId(span),
-                color: color,
-                size: span.duration
-            });
-        }
-    };
-    const addLink = (sourceSpan, destinationSpan) => {
-        const link = {
-            source: getUniqueNodeId(sourceSpan),
-            target: getUniqueNodeId(destinationSpan)
         };
-        if (sourceSpan.hasError() || destinationSpan.hasError()) {
-            link.color = colorGenerator.getColor(ColorGenerator.ERROR);
-        }
-        links.push(link);
-    };
-    rootSpan.walk((span, data) => {
-        let linkSource = data;
-        if (linkSource && span.kind === Constants.Span.Kind.SERVER) { // Ending link traversing
-            addNodeIfNotPresent(span);
-            addLink(linkSource, span);
-            linkSource = null;
-        } else if (!linkSource && span.kind === Constants.Span.Kind.CLIENT) { // Starting link traversing
-            addNodeIfNotPresent(span);
-            linkSource = span;
-        }
-        return linkSource;
-    }, null);
+        const addLink = (sourceSpan, destinationSpan) => {
+            const link = {
+                source: getUniqueNodeId(sourceSpan),
+                target: getUniqueNodeId(destinationSpan)
+            };
+            if (sourceSpan.hasError() || destinationSpan.hasError()) {
+                link.color = colorGenerator.getColor(ColorGenerator.ERROR);
+            }
+            links.push(link);
+        };
+        rootSpan.walk((span, data) => {
+            let linkSource = data;
+            if (linkSource && span.kind === Constants.Span.Kind.SERVER) { // Ending link traversing
+                addNodeIfNotPresent(span);
+                addLink(linkSource, span);
+                linkSource = null;
+            } else if (!linkSource && span.kind === Constants.Span.Kind.CLIENT) { // Starting link traversing
+                addNodeIfNotPresent(span);
+                linkSource = span;
+            }
 
-    return (
-        nodes.length > 0 && links.length > 0
-            ? (
-                <Graph id={"trace-dependency-graph"} className={classes.graph}
-                    data={{
-                        nodes: nodes,
-                        links: links
-                    }}
-                    config={{
-                        directed: true,
-                        nodeHighlightBehavior: true,
-                        highlightOpacity: 0.2,
-                        node: {
-                            fontSize: 15,
-                            highlightFontSize: 15
-                        },
-                        link: {
-                            strokeWidth: 3,
-                            highlightColor: "#444"
-                        },
-                        d3: {
-                            gravity: -200
-                        }
-                    }}
-                />
-            )
-            : null
-    );
-};
+            if (span.duration < minDuration) {
+                minDuration = span.duration;
+            }
+            if (span.duration > maxDuration) {
+                maxDuration = span.duration;
+            }
+            return linkSource;
+        }, null);
+
+        return (
+            nodes.length > 0 && links.length > 0
+                ? (
+                    <Graph id={"trace-dependency-graph"} className={classes.graph}
+                        data={{
+                            nodes: nodes,
+                            links: links
+                        }}
+                        config={{
+                            directed: true,
+                            nodeHighlightBehavior: true,
+                            highlightOpacity: 0.2,
+                            node: {
+                                fontSize: 15,
+                                highlightFontSize: 15,
+                                viewGenerator: (node) => {
+                                    const radius = (((node.span.duration - minDuration) * (MAX_RADIUS - MIN_RADIUS))
+                                           / (maxDuration - minDuration)) + MIN_RADIUS;
+
+                                    let nodeSVGContent;
+                                    const circle = <circle cx="120" cy="120" r={radius} fill={node.color}/>;
+                                    if (node.span.hasError()) {
+                                        nodeSVGContent = (
+                                            <g>
+                                                <g>
+                                                    <g>
+                                                        {circle}
+                                                    </g>
+                                                </g>
+                                                <g transform="translate(0, 0)">
+                                                    <path stroke="#fff" strokeWidth="3"
+                                                        fill={colorGenerator.getColor(ColorGenerator.ERROR)}
+                                                        d="M146.5,6.1c-23.6,0-42.9,19.3-42.9,42.9s19.3,42.9,42.9,
+                                                           42.9s42.9-19.3,42.9-42.9S170.1,6.1, 146.5,6.1z"/>
+                                                    <path fill="#ffffff"
+                                                        d="M144.6,56.9h7.9v7.9h-7.9V56.9z
+                                                           M144.6,25.2h7.9V49h-7.9V25.2z"/>
+                                                </g>
+                                            </g>
+                                        );
+                                    } else {
+                                        nodeSVGContent = circle;
+                                    }
+                                    return (
+                                        <svg x="0" y="0" width="100%" height="100%" viewBox="0 0 240 240">
+                                            {nodeSVGContent}
+                                        </svg>
+                                    );
+                                }
+                            },
+                            link: {
+                                strokeWidth: 3,
+                                highlightColor: "#444"
+                            },
+                            d3: {
+                                gravity: -200
+                            }
+                        }}
+                    />
+                )
+                : null
+        );
+    };
+
+}
 
 DependencyDiagram.propTypes = {
     classes: PropTypes.object.isRequired,
