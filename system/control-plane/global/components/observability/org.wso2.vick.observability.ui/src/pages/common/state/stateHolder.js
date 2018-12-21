cb28ccbde21b8e1251cf6543ee37d3accabc57a5
sinthuja <sinthuja@wso2.com>
2018-12-21 15:23:44 +0530

    Add sp worker ingress for dashboard access.

--- system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/pages/common/state/stateHolder.js
+++ system/control-plane/global/components/observability/org.wso2.vick.observability.ui/src/pages/common/state/stateHolder.js
@@ -175,7 +175,7 @@ class StateHolder {
         return new Promise((resolve) => {
             // TODO : Load configuration from server
             const loadedConfiguration = {
-                observabilityAPIURL: "http://localhost:9123/api",
+                observabilityAPIURL: "http://wso2sp-observability-api/api",
                 percentageRangeMinValue: {
                     errorThreshold: 0.5,
                     warningThreshold: 0.7
