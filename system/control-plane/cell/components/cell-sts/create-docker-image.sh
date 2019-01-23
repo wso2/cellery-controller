50368bef666e2be05d81aa1d3535e9853e0df3cf
Miraj Abeysekara <miraj@wso2.com>
2019-01-23 13:06:17 +0530

    Update sts docker tag to :test

--- system/control-plane/cell/components/cell-sts/create-docker-image.sh
+++ system/control-plane/cell/components/cell-sts/create-docker-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mvn clean install
-docker build -t wso2vick/cell-sts:envoy-cell-gw .
-docker push wso2vick/cell-sts:envoy-cell-gw
+docker build -t wso2vick/cell-sts:test .
+docker push wso2vick/cell-sts:test
 
