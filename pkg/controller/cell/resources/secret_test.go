02247049d323b1d0e316859074e5af6581e52879
Izza <isurulucky@gmail.com>
2019-05-03 15:25:10 +0530

    remove an unused method

--- pkg/controller/cell/resources/secret_test.go
+++ pkg/controller/cell/resources/secret_test.go
@@ -19,14 +19,7 @@
 package resources
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
 	"testing"
-	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -53,30 +46,4 @@ func TestCreateKeyPairSecret(t *testing.T) {
 	if CreateKeyPairSecret(cell) == nil {
 		t.Errorf("Secret for cell with spec: %v not created properly", cell.Spec)
 	}
-}
-
-func createPrivateKeyAndCert(cell *v1alpha1.Cell) (*pem.Block, *pem.Block) {
-
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	template := x509.Certificate{
-		SerialNumber: big.NewInt(1),
-		Subject: pkix.Name{
-			Organization: []string{cell.Name},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(time.Hour * 24 * 180),
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	certBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, privateKey.Public(), privateKey)
-
-	return &pem.Block{
-			Type:    "RSA PRIVATE KEY",
-			Headers: nil,
-			Bytes:   x509.MarshalPKCS1PrivateKey(privateKey),
-		},
-		&pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
 }
