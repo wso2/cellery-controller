d65bc9a4a9050e002e8b7c0a2de71d33f976ec22
Miraj Abeysekara <miraj@wso2.com>
2019-07-03 19:33:26 +0530

    Fix resource request and limits propagation issue

--- pkg/controller/service/resources/deployment.go
+++ pkg/controller/service/resources/deployment.go
@@ -41,19 +41,19 @@ func CreateServiceDeployment(service *v1alpha1.Service) *appsv1.Deployment {
 	//https://github.com/istio/istio/blob/master/install/kubernetes/helm/istio/templates/sidecar-injector-configmap.yaml
 
 	if service.Spec.Resources.Limits != nil {
+		m := make(map[corev1.ResourceName]resource.Quantity)
 		for k, v := range service.Spec.Resources.Limits {
-			service.Spec.Container.Resources.Limits = corev1.ResourceList{
-				corev1.ResourceName(k): resource.MustParse(v.Amount),
-			}
+			m[corev1.ResourceName(k)] = resource.MustParse(v.Amount)
 		}
+		service.Spec.Container.Resources.Limits = m
 	}
 
 	if service.Spec.Resources.Requests != nil {
+		m := make(map[corev1.ResourceName]resource.Quantity)
 		for k, v := range service.Spec.Resources.Requests {
-			service.Spec.Container.Resources.Requests = corev1.ResourceList{
-				corev1.ResourceName(k): resource.MustParse(v.Amount),
-			}
+			m[corev1.ResourceName(k)] = resource.MustParse(v.Amount)
 		}
+		service.Spec.Container.Resources.Requests = m
 	}
 
 	return &appsv1.Deployment{
@@ -131,19 +131,20 @@ func buildContainerWithReadinessProbe(service *v1alpha1.Service) corev1.Containe
 	service.Spec.Container.ReadinessProbe = readinessProbe
 
 	if service.Spec.Resources.Limits != nil {
+		m := make(map[corev1.ResourceName]resource.Quantity)
 		for k, v := range service.Spec.Resources.Limits {
-			service.Spec.Container.Resources.Limits = corev1.ResourceList{
-				corev1.ResourceName(k): resource.MustParse(v.Amount),
-			}
+			m[corev1.ResourceName(k)] = resource.MustParse(v.Amount)
 		}
+		service.Spec.Container.Resources.Limits = m
 	}
 
 	if service.Spec.Resources.Requests != nil {
+		m := make(map[corev1.ResourceName]resource.Quantity)
 		for k, v := range service.Spec.Resources.Requests {
-			service.Spec.Container.Resources.Requests = corev1.ResourceList{
-				corev1.ResourceName(k): resource.MustParse(v.Amount),
-			}
+			m[corev1.ResourceName(k)] = resource.MustParse(v.Amount)
 		}
+		service.Spec.Container.Resources.Requests = m
 	}
+
 	return service.Spec.Container
 }
