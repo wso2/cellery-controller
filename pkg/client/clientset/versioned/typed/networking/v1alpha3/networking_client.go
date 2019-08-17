c98b5a7a303c19c1413b58d34b6cbbc242f7cec4
Miraj Abeysekara <miraj@wso2.com>
2019-08-17 08:34:12 +0000

    Update k8s version to 1.15.2

--- pkg/client/clientset/versioned/typed/networking/v1alpha3/networking_client.go
+++ pkg/client/clientset/versioned/typed/networking/v1alpha3/networking_client.go
@@ -23,7 +23,6 @@ package v1alpha3
 import (
 	v1alpha3 "github.com/cellery-io/mesh-controller/pkg/apis/istio/networking/v1alpha3"
 	"github.com/cellery-io/mesh-controller/pkg/client/clientset/versioned/scheme"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -88,7 +87,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1alpha3.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
--- pkg/client/clientset/versioned/typed/serving/v1beta1/serving_client.go
+++ pkg/client/clientset/versioned/typed/serving/v1beta1/serving_client.go
@@ -23,7 +23,6 @@ package v1beta1
 import (
 	v1beta1 "github.com/cellery-io/mesh-controller/pkg/apis/knative/serving/v1beta1"
 	"github.com/cellery-io/mesh-controller/pkg/client/clientset/versioned/scheme"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -88,7 +87,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1beta1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
